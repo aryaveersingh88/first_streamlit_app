6ee34832c50117c052323e0a8edec49a988f35c2
aryaveersingh88 <41035827+aryaveersingh88@users.noreply.github.com>
2023-10-16 14:54:17 -0400

    Update streamlit_app.py
    Second entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,7 @@ streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 #Allow end user to add a fruit to the list 
-# add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
-# streamlit.write('The user enetered', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding', add_my_fruit)
+# fruityvice_add = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+
